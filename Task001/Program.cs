3956e46968f21cbb1baa43f0a7c1a7d5267ce509
Anna <ankakras@yandex.ru>
2022-10-14 18:23:18 +0700

    Решение задачи 001

---
+++ Task001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задать массив из 8 элементов, заполнить нулями и единицами, вывести их на экран.
+void GenerateArrayElements(int[]array, int min, int max)
+{
+    Random random = new Random();
+    for(int i = 0; i < array.Length; i++)
+        array[i] = random.Next(min, max);
+}
+void PrintArray(int[]array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]}");
+    Console.WriteLine();
+}
+int length = 8;
+int[] array = new int[length];
+GenerateArrayElements(array, 0, 2);
+Console.WriteLine("Ваш массив: ");
+PrintArray(array);
+
