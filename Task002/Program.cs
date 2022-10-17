d7ea9a8a7f97552db3ff7e83755d44c3a18111ad
Anna <ankakras@yandex.ru>
2022-10-17 19:32:10 +0700

    Решение

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,4 +1,5 @@
-﻿// Задать массив из 12 элементов, заполнить числами  из [0,9]. 
+﻿
+// Задать массив из 12 элементов, заполнить числами  из [0,9]. 
 //Найти сумму положительных/отрицательных элементов массива.
 
 void GenerateArrayElements(int[]array, int min, int max)
@@ -13,24 +14,28 @@ void PrintArray(int[]array)
         Console.Write($"{array[i]}");
     Console.WriteLine();
 }
-
+int SumPositiveElements(int[]array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) sum = sum + array[i];
+    }
+    return sum;
+}
+int SumNegativeElements(int[]array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < 0) sum = sum + array[i];
+    }
+    return sum;
+}
 int length = 12;
 int[] array = new int[length];
 GenerateArrayElements(array, 0, 10);
 Console.WriteLine("Ваш массив: ");
 PrintArray(array);
-int sum = 0;
-int sum2 = 0;
-    for(int i = 0; i < array.Length; i++)
-    {
-        if(array[i] > 0) sum = sum + array[i];
-        else
-        {
-            sum2 = sum2 + array[i];
-        }
-    }
-Console.WriteLine($"Сумма положительных элементов массива: {sum}");
-Console.WriteLine($"Сумма отрицательных элементов массива: {sum2}");
-
-
-
+Console.WriteLine($"Сумма положительных элементов массива: {SumPositiveElements(array)}");
+Console.WriteLine($"Сумма отрицательных элементов массива: {SumNegativeElements(array)}");
