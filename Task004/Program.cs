a34b31e06e87abd6b6761899bb36b7bba866e9ec
Anna <ankakras@yandex.ru>
2022-10-17 18:55:06 +0700

    Решение

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -4,18 +4,35 @@ void GenerateArrayElements(int[]array)
 {
     Random random = new Random();
     for(int i = 0; i < array.Length; i++)
-        array[i] = random.Next(1,10);
+        array[i] = random.Next(10,1000);
 }
 void PrintArray(int[]array)
 {
     for(int i = 0; i < array.Length; i++)
-        Console.Write($"{array[i]}");
+        Console.Write($"{array[i]} ");
     Console.WriteLine();
 }
+
 int length = 123;
 int[] array = new int[length];
 Console.WriteLine("Ваш массив - ");
 GenerateArrayElements(array);
 PrintArray(array);
 Console.WriteLine();
-//ДОДЕЛАТЬ!!!!
+int n = 0;
+int i = 10;
+int x = 0;
+int y = 0;
+while(i < 100)
+{
+    n = array[i];
+    while(n > 0)
+    {
+        x++;
+        n = n / 10;
+    }
+    y = y + x;
+    i++;
+}
+Console.WriteLine("Количество элементов из отрезка [10,99] - " + x);
+///!!!!ДОДЕЛАТЬ!!!
