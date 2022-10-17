db5e394ae82f7545d5dcd894b9bd8a15792ccbeb
Anna <ankakras@yandex.ru>
2022-10-17 19:17:04 +0700

    Решение

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -12,6 +12,25 @@ void PrintArray(int[]array)
         Console.Write($"{array[i]} ");
     Console.WriteLine();
 }
+void CountElementsArray(int[]array)
+{
+    int n = 0;
+    int i = 10;
+    int y = 0;
+    while(i < 100)
+    {
+        n = array[i];
+        int x = 0;
+        while(n > 0)
+        {
+            x++;
+            n = n / 10;
+        }
+        y = y + x;
+        i++;
+    }
+Console.WriteLine("Количество элементов из отрезка [10,99] - " + y);
+}
 
 int length = 123;
 int[] array = new int[length];
@@ -19,20 +38,4 @@ Console.WriteLine("Ваш массив - ");
 GenerateArrayElements(array);
 PrintArray(array);
 Console.WriteLine();
-int n = 0;
-int i = 10;
-int x = 0;
-int y = 0;
-while(i < 100)
-{
-    n = array[i];
-    while(n > 0)
-    {
-        x++;
-        n = n / 10;
-    }
-    y = y + x;
-    i++;
-}
-Console.WriteLine("Количество элементов из отрезка [10,99] - " + x);
-///!!!!ДОДЕЛАТЬ!!!
+CountElementsArray(array);
